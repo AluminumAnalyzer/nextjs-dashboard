3c298c3bcb15170acf4252f39ae425ea9ab54209
AluminumAnalyzer <blueunicon@gmail.com>
2023-12-06 23:14:24 +0900

    Search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
     console.log('before check if', term);
     if (term) {
